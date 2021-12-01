534a8eb8e1759f4eabc51efe33236d457bf82c78
Sriharsh Dubale <dsriharsh@gmail.com>
2021-12-01 21:47:53 +0530

    more additions

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import { useState } from "react";
 import Navbar from "./components/Navbar";
 import People from "./components/People";
 import Planets from "./components/Planets";
-
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+const queryClient = new QueryClient();
 function App() {
   const [page, setPage] = useState("planets");
   return (
-    <div className="App">
-      <h1>Star Wars Info</h1>
-      <Navbar setPage={setPage} />
-      <div className="content">
-        {page === "planets" ? <Planets /> : <People />}
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Star Wars Info</h1>
+        <Navbar setPage={setPage} />
+        <div className="content">
+          {page === "planets" ? <Planets /> : <People />}
+        </div>
       </div>
-    </div>
+    </QueryClientProvider>
   );
 }
 
--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,6 +1,14 @@
 import React from "react";
+import { useQuery } from "react-query";
+
+const fetchPlanets = async () => {
+  const res = await fetch("http://swapi.dev/api/planets/");
+  return res.json();
+};
 
 function Planets() {
+  const { data, isLoading, status } = useQuery("planets", fetchPlanets);
+  console.log("data", data);
   return (
     <div>
       <h2>Planets</h2>
