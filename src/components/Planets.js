92d1aef3d5d314f67774d297993d500e79514103
Sriharsh Dubale <dsriharsh@gmail.com>
2021-12-01 22:40:48 +0530

    lesson 4

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,21 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "react-query";
 import Planet from "./Planet";
 
-const fetchPlanets = async () => {
-  const res = await fetch("http://swapi.dev/api/planets/");
+const fetchPlanets = async ({ queryKey }) => {
+  const res = await fetch(
+    `http://swapi.dev/api/planets/?page=${queryKey[1].page}`
+  );
   return res.json();
 };
 
 function Planets() {
-  const { data, status } = useQuery("planets", fetchPlanets, {
-    staleTime: 0,
-    cacheTime: 10,
-  });
+  const [page, setPage] = useState(1);
+  const { data, status } = useQuery(["planets", { page }], fetchPlanets);
 
   return (
     <div>
       <h2>Planets</h2>
+      <button onClick={() => setPage(page - 1)}>Previous</button>
+      <button onClick={() => setPage(page + 1)}>Next</button>
+
       {status === "loading" && <div>Loading...</div>}
       {status === "error" && <div>Error</div>}
       {status === "success" && (
